ab271f5a0ab75b619912ff3d4e1beae5c5d20a4e
DominikTworek <Dominikt123@gmail.com>
2019-01-22 07:52:42 +0100

    small update profile user

--- src/mainWindowTrainer/ProfileT.java
+++ src/mainWindowTrainer/ProfileT.java
@@ -2,16 +2,30 @@ package mainWindowTrainer;
 
 import LoginWindow.LoadLoginWindow;
 import LoginWindow.LoginController;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import utilities.UserService;
 
+import java.io.*;
 import java.net.URL;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.rmi.RemoteException;
 import java.util.ResourceBundle;
 
 public class ProfileT implements Initializable {
+
+    @FXML
+    private ImageView image;
 
     @FXML
     private JFXTextField plec;
@@ -31,6 +45,14 @@ public class ProfileT implements Initializable {
     @FXML
     private JFXTextField inne;
 
+    @FXML
+    private JFXButton addImage;
+
+    private String tmp = "";
+
+    @FXML
+    private Text textImage;
+
     private Long IDuser = LoginController.getIDuser();
 
     private UserService userService = LoadLoginWindow.getUserService();
@@ -40,7 +62,7 @@ public class ProfileT implements Initializable {
         setInformation();
     }
 
-    void setInformation(){
+    void setInformation() {
         try {
             imie.setText(userService.getFieldFromUser(IDuser, "Imie"));
             nazwisko.setText(userService.getFieldFromUser(IDuser, "Nazwisko"));
@@ -49,5 +71,62 @@ public class ProfileT implements Initializable {
         } catch (RemoteException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    void addImage(ActionEvent event) {
+        try {
+            String nazwaZdjecia = userService.getFieldFromUser(IDuser, "Login");
+            FileChooser fc = new FileChooser();
+            File seletedFile = fc.showOpenDialog(null);
+            String png = ".png";
+            String jpg = ".jpg";
+            Boolean zawiera;
+            Boolean zawiera2;
+            if (seletedFile != null) {
+                tmp = seletedFile.getAbsolutePath();
+                zawiera = tmp.contains(png);
+                zawiera2 = tmp.contains(jpg);
+                if (zawiera) {
+                    try {
+                        reloadImage();
+                        Files.copy(Paths.get(tmp), Paths.get("src/TrainerImages/" + nazwaZdjecia + ".png"), StandardCopyOption.REPLACE_EXISTING);
+                        textImage.setText("Zdjęcie Dodane");
+                        loadImage();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    textImage.setText("Format akceptowalny zdjęcia to PNG/JPG.");
+                }
+                if (zawiera2) {
+                    try {
+                        reloadImage();
+                        Files.copy(Paths.get(tmp), Paths.get("src/TrainerImages/"+nazwaZdjecia + ".jpg"), StandardCopyOption.REPLACE_EXISTING);
+                        textImage.setText("Zdjęcie Dodane");
+                        loadImage();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    textImage.setText("Format akceptowalny zdjęcia to PNG/JPG.");
+                }
+            } else {
+                textImage.setText("Błąd w dodawaniu zdjęcia. Dodaj Jeszcze raz.");
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+
+    void loadImage() throws IOException {
+        String nazwaZdjecia = userService.getFieldFromUser(IDuser, "Login");
+        Image image1 = new Image(new FileInputStream("src/TrainerImages/"+nazwaZdjecia + ".jpg"));
+        image.setImage(image1);
+    }
+
+    void reloadImage(){
+        image.setImage(null);
     }
 }
